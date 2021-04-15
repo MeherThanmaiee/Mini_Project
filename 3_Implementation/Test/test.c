ac64fd8d79f6be8425375eb8f1dc85a0d1f2e1af
MeherThanmaiee <62166597+MeherThanmaiee@users.noreply.github.com>
2021-04-15 22:43:14 +0530

    Update test.c

--- 3_Implementation/Test/test.c
+++ 3_Implementation/Test/test.c
@@ -1,14 +1,6 @@
 #include "fun.h"
 #include "unity.h"
- char name[20];
-  char address[50];
-  char phno[10];
-  int period;
-  char name[20];
- int roomno;
- char roomtype[20];
- char foodtype[20];
-int bill;
+
 /* Required by the unity test framework */
 void setUp()
 {
@@ -17,48 +9,71 @@ void setUp()
 void tearDown()
 {
 }
+struct customer
+{
+  char name[20];
+  char address[50];
+  char phno[10];
+  int period;
+}c;
+struct allocdealloc
+{
+ char name[20];
+ int roomno;
+ char roomtype[20];
+ char foodtype[20];
+ int year;
+ int month;
+ int day;
+ int hour;
+ int minute;
+ }a;
+struct billing
+{
+ int bill;
+ }b;
 void test_avail(void){
-
-    TEST_ASSERT_EQUAL(2,100);
+    int count=100
+    TEST_ASSERT_EQUAL(1,avail(count));
     
 }
 void test_customer_details(void){
-    strcpy(name,"meher");
-    strcpy(address,"vijayawada");
-    strcpy(phno,"8367230297");
-    strcpy(period,"3");
+    strcpy(c.name,"meher");
+    strcpy(c.address,"vijayawada");
+    strcpy(c.phno,"8367230297");
+    strcpy(c.period,"3");
 
-TEST_ASSERT_EQUAL_STRING("name",name);
-TEST_ASSERT_EQUAL_STRING("address",address);
-TEST_ASSERT_EQUAL_STRING("phno",phno);
-TEST_ASSERT_EQUAL_STRING("period",period);
+TEST_ASSERT_EQUAL_STRING("meher",c.name);
+TEST_ASSERT_EQUAL_STRING("vijayawada",c.address);
+TEST_ASSERT_EQUAL_STRING("8367230297",c.phno);
+TEST_ASSERT_EQUAL_STRING("3",c.period);
 }
 void test_room_alloc(){
-     strcpy(name,"meher");
-    strcpy(roomno,"4");
-    strcpy(roomtype,"delux");
-    strcpy(foodtype,"non_veg");
+     strcpy(a.name,"meher");
+    strcpy(a.roomno,"4");
+    strcpy(a.roomtype,"delux");
+    strcpy(a.foodtype,"non_veg");
 
-TEST_ASSERT_EQUAL_STRING("name",name);
-TEST_ASSERT_EQUAL_STRING("roomno",roomno);
-TEST_ASSERT_EQUAL_STRING("roomtype",roomtype);
-TEST_ASSERT_EQUAL_STRING("foodtype",foodtype);
+TEST_ASSERT_EQUAL_STRING("meher",name);
+TEST_ASSERT_EQUAL_STRING("4",roomno);
+TEST_ASSERT_EQUAL_STRING("delux",roomtype);
+TEST_ASSERT_EQUAL_STRING("non_veg",a.foodtype);
 }
 void test_room_dealloc(){
-     strcpy(name,"meher");
-    strcpy(roomno,"4");
+     strcpy(c.name,"meher");
+    strcpy(c.roomno,"4");
     
  
 
-TEST_ASSERT_EQUAL_STRING("name",name);
-TEST_ASSERT_EQUAL_STRING("roomno",roomno);
+TEST_ASSERT_EQUAL_STRING("meher",c.name);
+TEST_ASSERT_EQUAL_STRING("4",c.roomno);
 
 }
 void test_billing(){
-     strcpy(name,"meher");
-    strcpy(roomno,"4");
+     strcpy(b.name,"meher");
+    strcpy(b.roomno,"4");
 int bill=7200;
-TEST_ASSERT_EQUAL_STRING("name",name);
+TEST_ASSERT_EQUAL_STRING("meher",b.name);
 TEST_ASSERT(bill==7200);
 
 }
